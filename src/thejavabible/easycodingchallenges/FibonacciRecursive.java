776ce88d67beecfc8bd113a6c7a9fc5b0777d1e0
martynfigueiredo <martynfigueiredo@outlook.com>
2022-10-27 17:54:00 -0300

    FibonacciRecursive

---
+++ src/thejavabible/easycodingchallenges/FibonacciRecursive.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package thejavabible.easycodingchallenges;
+
+/**
+ *
+ * @author marty
+ */
+public class FibonacciRecursive {
+
+    public static long fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.printf("%d", fibonacci(10));
+    }
+}
